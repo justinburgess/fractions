a1c1bc4be3e91110a400f7d4c39d2089b518d639
justinburgess <36819604+justinburgess@users.noreply.github.com>
2025-02-09 07:26:28 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,44 @@
-
 public class Main {
+
     public static void main(String[] args) {
-        EgyptianFraction fraction1 = new EgyptianFraction(args[0]);
-        EgyptianFraction fraction2 = new EgyptianFraction(args[1]);
-        System.out.println("Fraction 1 decomposition is: " + fraction1.getDecomposition());
-        System.out.println("Fraction 1 decomposition is valid: " + fraction1.isValidDecomposition());
-        System.out.println("Fraction 2 decomposition is: " + fraction2.getDecomposition());
-        System.out.println("Fraction 2 decomposition is valid: " + fraction2.isValidDecomposition());
+        String operation = getOperation(args[1]);
+        // create fractions
+        Fraction fraction1 = new Fraction(args[0]);
+        Fraction fraction2 = new Fraction(args[2]);
+
+        // run arguments against the fractions
+        String result = runOperation(fraction1, fraction2, operation);
+
+        // output the result
+        System.out.println(result);
+    }
+
+    private static String getOperation(String argument) {
+        return switch (argument.toLowerCase()) {
+            case "add", "+" -> "add";
+            case "subtract", "minus", "-" -> "subtract";
+            case "/", "\\", "u+00f7", "divide", "division" -> "divide";
+            case "*", "x", "by", "multipliedby" -> "multiply";
+            default -> throw new IllegalArgumentException("Invalid operation submitted: " + argument);
+        };
+    }
+
+    private static String runOperation (Fraction fraction1, Fraction fraction2, String operator) {
+        return switch (operator) {
+            case "add":
+                fraction1.add(fraction2);
+                yield fraction1.getFraction();
+            case "subtract":
+                fraction1.subtract(fraction2);
+                yield fraction1.getFraction();
+            case "multiply":
+                fraction1.multiplyBy(fraction2);
+                yield fraction1.getFraction();
+            case "divide":
+                fraction1.divideBy(fraction2);
+                yield fraction1.getFraction();
+            default:
+                throw new IllegalArgumentException("Invalid runtime operation: " + operator);
+        };
     }
 }
